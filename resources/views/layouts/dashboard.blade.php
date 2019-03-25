601668623b71d975a21090d4bc7f80c79e25e1ed
cherlow <paulcherlow@gmail.com>
2019-03-25 21:48:14 +0300

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -9,6 +9,7 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
     <!-- <link href="{{ asset('css/w3.css') }}" rel="stylesheet"> -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <!-- Font Awesome -->
   <link href="{{ asset('css/fa/css/font-awesome.min.css') }}" rel="stylesheet">
